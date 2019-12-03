3aaabb07a982b31ef0c52cf827609052b6193514
fernando martinez <fmartinezr195@gmail.com>
2019-12-03 07:47:52 -0400

    agregados cambios en route

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
 //Route::resource('Libro', 'LibroController'); // Esto permite llamar al controlador del libro para hacer las respectivas llamadas  localhost8000/libro
 Route::get('/Libro','LibroController@index');
 Route::get('/Libro/create','LibroController@create');
+Route::post('/Libro','LibroController@store');
 Route::get('/Libro/{cod}','LibroController@destroy');
