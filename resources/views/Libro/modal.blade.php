f94d3b4f845d8a8aa7dda62c5342011cbb71f458
Jose Manuel Gonzalez <33811865+Josestadele@users.noreply.github.com>
2019-12-02 23:54:09 -0400

    Pantalla emergente para confirmar el eliminar ... ver que pasa que no lo toma

---
+++ resources/views/Libro/modal.blade.php
@@ -0,0 +1,29 @@
+<div class="modal fade modal-slide-in-right" aria-hidden="true" role="dialog" tabindex="-1" id="modal-delete-{{$lib->cod}}">
+
+{{Form::Open(array('action'=>array('LibroController@destroy',$lib->cod),'method'=>'delete'))}}
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <div class="modal-header">
+
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">x</span>
+                    </button>
+
+                <h4 class="modal-title"> Eliminar Categoria</h4>
+            </div>
+
+            <div class="modal-body">
+                    <p>Confirme si desea Eliminar El libro</p>
+            </div>
+               
+
+            <div class="modal-footer">
+                 <button type="button" class="btn btn-default" data-dismiss="modal"> Cerrar </button>
+                 <button type="submit" class="btn btn-primary" data-dismiss="modal"> Confirmar </button>
+            </div>
+        
+        </div>
+    </div>
+{{Form::Close()}}
+</div>
