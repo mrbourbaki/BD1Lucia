4a78b6002554b9c6cd2ed394631d63f1f7bd8718
Omar <omaralberto6@gmail.com>
2019-12-05 10:59:12 -0400

    Syntax modal

--- resources/views/Libro/modal.blade.php
+++ resources/views/Libro/modal.blade.php
@@ -5,23 +5,21 @@
 
             <div class="modal-header">
 
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">x</span>
-                    </button>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">x</span>
+                </button>
 
-                <h4 class="modal-title"> Eliminar Libro : {{$lib->titulo_original}} </h4>
+                <h4 class="modal-title"> Eliminar Libro: {{$lib->titulo_original}}</h4>
             </div>
 
             <div class="modal-body">
-                    <p>Confirme si desea Eliminar El libro</p>
+                <p>Esta seguro que desea eliminar '{{$lib->titulo_original}}'?</p>
             </div>
-               
 
             <div class="modal-footer">
-                 <button type="button" class="btn btn-default" data-dismiss="modal"> Cerrar </button>
-                 <a href="/Libro/{{$lib->cod}}"><button class="btn btn-primary"> Eliminar</button></a>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                <a href="/Libro/{{$lib->cod}}"><button class="btn btn-primary">Eliminar</button></a>
             </div>
-        
         </div>
     </div>
 </div>
